c8ce533bd4e4b9f0416395e94c01b7d2f3bf52e0
Robert <48084300+Robbiexd@users.noreply.github.com>
2020-05-08 01:15:49 +0200

    SendMail page added

---
+++ DBLite/DBLite/Views/SendMailPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DBLite.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SendMailPage : ContentPage
+    {
+        public SendMailPage()
+        {
+            InitializeComponent();
+        }
+
+        void btnSend_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("susairajs18@gmail.com");
+                //mail.To.Add(txtTo.Text);
+                //mail.Subject = txtSubject.Text;
+                //mail.Body = txtBody.Text;
+
+                //pozn - tutorial nefunguje
+
+                SmtpServer.Port = 587;
+                SmtpServer.Host = "smtp.gmail.com";
+                SmtpServer.EnableSsl = true;
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("susairajs18@gmail.com", "***********");
+
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Faild", ex.Message, "OK");
+            }
+        }
+    }
+}
